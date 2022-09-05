94c5ada3e78f506b2cdc26dba45c32855632d403
mangiu90 <giudicimanuel@gmail.com>
2022-09-05 06:03:54 -0300

    config db

---
+++ config/db.js
@@ -0,0 +1,22 @@
+import { Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+})
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
+    await sequelize.sync({ force: true });
+}
+
+export {
+    sequelize,
+    connectDB,
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,8 @@ app.use(express.json());
 dotenv.config();
 
 (async () => {
+    await connectDB();
+
     const port = process.env.PORT || 4000;
     app.listen(port, () => {
         console.log(`Listening on port ${port}`)
