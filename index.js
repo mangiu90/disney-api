f1fbee9f21ddde17008c666e7e31a9263c4f9073
mangiu90 <giudicimanuel@gmail.com>
2022-09-05 21:59:19 -0300

    add category seed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import { Category } from './models/index.js';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,21 @@ dotenv.config();
 
 (async () => {
     await connectDB();
+
+    await Category.bulkCreate([
+        {
+            img_url: 'https://www.filmsite.org/images/crimefilms.gif',
+            name: 'Action',
+        },
+        {
+            img_url: 'https://www.filmsite.org/images/comedyfilms.gif',
+            name: 'Comedy',
+        },
+        {
+            img_url: 'https://www.filmsite.org/images/dramafilms.gif',
+            name: 'Drama',
+        }
+    ]);
 
     app.use('/auth', authRoutes);
 
